eb8c6fdc5bdbcd338f01d5858694cd9b6fc9ad07
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2024-02-06 15:48:17 +0530

    App CRUD operation done and tested.

--- coupon-generator-backend/src/main/java/org/example/controller/AppController.java
+++ coupon-generator-backend/src/main/java/org/example/controller/AppController.java
@@ -1,11 +1,44 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.dto.AppDTO;
+import org.example.entity.AppEntity;
+import org.example.service.AppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/app")
 public class AppController {
+
+    @Autowired
+    AppService appService;
+
+    @PostMapping("/create-app")
+    public ResponseEntity<String> createApp(@RequestBody AppDTO appDTO){
+        appService.createApp(appDTO);
+        return new ResponseEntity<>("App Created", HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all-apps")
+    public ResponseEntity<List<AppEntity>> getAllApps(){
+        List<AppEntity> appEntities = appService.getAllApps();
+        return new ResponseEntity<>(appEntities, HttpStatus.OK);
+    }
+
+    @PutMapping("/update-app/{appId}")
+    public ResponseEntity<String> updateApp(@RequestBody AppDTO appDTO, @PathVariable int appId){
+        appService.updateApp(appDTO, appId);
+        return new ResponseEntity<>("App Updated", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-app/{appId}")
+    public ResponseEntity<String> deleteApp(@PathVariable int appId){
+        appService.deleteApp(appId);
+        return new ResponseEntity<>("App Deleted : "+appId, HttpStatus.OK);
+    }
 }
