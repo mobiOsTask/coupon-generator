7b5ca23ea54785509c7ee9e31a8e88e00e2326e1
ushalka kapugamage <ushalkakapugamage@gmail.com>
2024-02-07 15:30:18 +0530

    Added validations

--- coupon-generator-backend/src/main/java/org/example/service/impl/CouponServiceImpl.java
+++ coupon-generator-backend/src/main/java/org/example/service/impl/CouponServiceImpl.java
@@ -13,8 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.github.curiousoddman.rgxgen.RgxGen;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class CouponServiceImpl implements CouponService {
@@ -28,47 +27,102 @@ public class CouponServiceImpl implements CouponService {
     @Autowired
     AppRepository appRepository;
 
-    @Override
-    public void createCoupon(DTO dto) {
-        AppEntity appEntity = new AppEntity();
-        appEntity.setAppId(dto.getAppId());
-        appEntity.setWebSiteURL("www.testWeb.com");
-        appEntity.setContactNumber("0707777777");
-        appRepository.save(appEntity);
+    public void processData(DTO dto) {
+
+        if (validateDto(dto)) {
+            AppEntity appEntity = new AppEntity();
+            appEntity.setAppId(dto.getAppId());
+            appEntity.setWebSiteURL("www.testWeb.com");
+            appEntity.setContactNumber("0707777777");
+            appRepository.save(appEntity);
 
 
-        CampaignEntity campaignEntity = new CampaignEntity();
-        campaignEntity.setAppEntity(appEntity);
-        campaignEntity.setCampaignName(dto.getCampaignName());
-        campaignEntity.setStartDate(dto.getStartDate());
-        campaignEntity.setEndDate(dto.getEndDate());
-        campaignEntity.setCouponCount(dto.getCouponCount());
-        campaignRepository.save(campaignEntity);
+            CampaignEntity campaignEntity = new CampaignEntity();
+            campaignEntity.setAppEntity(appEntity);
+            campaignEntity.setCampaignName(dto.getCampaignName());
+            campaignEntity.setStartDate(dto.getStartDate());
+            campaignEntity.setEndDate(dto.getEndDate());
+            campaignEntity.setCouponCount(dto.getCouponCount());
+            campaignRepository.save(campaignEntity);
 
+            for (CouponDTO data : dto.getLogic()) {
+                List<CouponEntity> coupon = new ArrayList<>();
+                if (validateCouponDto(data)) {
+                    Set<String> strings = validateCode(data);
+                    for (String couponNumber : strings) {
+                        CouponEntity couponEntity = new CouponEntity();
+                        couponEntity.setCampaignEntity(campaignEntity);
+                        couponEntity.setCount(data.getCount());
+                        couponEntity.setType(data.getType());
+                        couponEntity.setAmount(data.getAmount());
+                        couponEntity.setDisplayValue(data.getDisplayValue());
+                        couponEntity.setUsageCount(data.isUsageCount());
+                        couponEntity.setLength(data.getLength());
+                        couponEntity.setRegex(data.getRegex());
+                        couponEntity.setNumber(couponNumber);
 
-        for (CouponDTO data : dto.getLogic()) {
-            List<CouponEntity> coupon = new ArrayList<>();
-            for (int i = 0; i < data.getCount(); i++) {
-                CouponEntity couponEntity = new CouponEntity();
-                couponEntity.setCampaignEntity(campaignEntity);
-                couponEntity.setCount(data.getCount());
-                couponEntity.setType(data.getType());
-                couponEntity.setAmount(data.getAmount());
-                couponEntity.setDisplayValue(data.getDisplayValue());
-                couponEntity.setUsageCount(data.isUsageCount());
-                couponEntity.setLength(data.getLength());
-                couponEntity.setRegex(data.getRegex());
-                couponEntity.setNumber(randomCode(data.getRegex(), data.getLength()));
+                        coupon.add(couponEntity);
+                    }
+                    couponRepository.saveAll(coupon);
+                }
 
-                coupon.add(couponEntity);
             }
-            couponRepository.saveAll(coupon);
+
         }
     }
 
+
     @Override
-    public List<CouponEntity> getCoupons() {
-        return couponRepository.findAll();
+    public void createCoupon(DTO dto) {
+        processData(dto);
+    }
+
+    public Set<String> validateCode(CouponDTO dto){
+
+        if (dto.getRegex().trim().isEmpty()) {
+            System.out.println(dto.getRegex());
+            throw new IllegalArgumentException("Regex cannot be null, empty, or contain only whitespace");
+        }
+        Set<String> codeSet = new HashSet<>();
+        while (codeSet.size() < dto.getCount()) {
+            codeSet.add(randomCode(dto.getRegex(), dto.getLength()));
+        }
+        return codeSet;
+    }
+
+    public boolean validateDto(DTO dto) {
+        int count = 0;
+        for (CouponDTO data : dto.getLogic()) {
+            count += data.getCount();
+        }
+        if (count != dto.getCouponCount() || dto.getCouponCount() < 0) {
+            throw new IllegalArgumentException("Invalid coupon count");
+        }
+
+        if (dto.getStartDate() == null || dto.getEndDate() == null) {
+            throw new IllegalArgumentException("Start date or End date cannot be empty");
+        }
+
+
+        if (dto.getStartDate().compareTo(dto.getEndDate()) > 0) {
+            throw new IllegalArgumentException("Start Date should be before the end date");
+        }
+        return true;
+    }
+
+    public boolean validateCouponDto(CouponDTO couponDto) {
+
+        List<String> values = Arrays.asList("rs", "point", "currency");
+
+        if (!values.contains(couponDto.getType().toLowerCase()) || couponDto.getRegex().trim().isEmpty()) {
+            throw new IllegalArgumentException("Type cannot contain values other than 'CURRENCY' and 'POINT'");
+        }
+
+
+        if (!values.contains(couponDto.getDisplayValue().toLowerCase()) || couponDto.getRegex().trim().isEmpty()) {
+            throw new IllegalArgumentException("Display Value cannot contain values other than 'RS' and 'Point'");
+        }
+        return true;
     }
 
     public String randomCode(String pattern, int length) {
@@ -78,4 +132,11 @@ public class CouponServiceImpl implements CouponService {
         return generatedString;
 
     }
+
+    @Override
+    public List<CouponEntity> getCoupons() {
+        return couponRepository.findAll();
+    }
+
+
 }
